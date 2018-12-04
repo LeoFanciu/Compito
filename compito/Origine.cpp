51962b00510188e1d158df3b16e4cc8c6c39765e
leonardo <wujinkang2000@gmail.com>
2018-12-04 09:38:06 +0100

    primo commit

---
+++ compito/Origine.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+#include <iostream>;
+
+void main() {
+	int counter = 0;
+
+	while (counter < 10) {
+
+		counter++;
+		cout << counter << " ";
+
+
+	}
+
+	system("PAUSE");
+
+
+}
